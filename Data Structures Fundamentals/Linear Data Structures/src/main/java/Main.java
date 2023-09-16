d05ee6c51cf280313c863f2a91cf0846b9b54d20
metodieva95 <metodievav@abv.bg>
2023-09-16 10:20:00 +0300

    update

--- Data Structures Fundamentals/Linear Data Structures/src/main/java/Main.java
+++ Data Structures Fundamentals/Linear Data Structures/src/main/java/Main.java
@@ -1,5 +1,16 @@
+import implementations.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(13);
+        numbers.add(25);
+        numbers.add(73);
+        numbers.add(100);
 
     }
 }
--- Data Structures Fundamentals/Linear Data Structures/src/main/java/implementations/ArrayList.java
+++ Data Structures Fundamentals/Linear Data Structures/src/main/java/implementations/ArrayList.java
@@ -7,53 +7,156 @@ import java.util.Iterator;
 
 public class ArrayList<E> implements List<E> {
 
+    private static final int INITIAL_CAPACITY = 4;
+    private Object[] elements;
+    private int size;
+    private int capacity = INITIAL_CAPACITY;
+
+    public ArrayList() {
+        this.elements = new Object[capacity];
+        this.size = 0;
+    }
+
     @Override
     public boolean add(E element) {
-        return false;
+
+        if (this.size == this.elements.length) {
+            grow();
+        }
+
+        this.elements[this.size] = element;
+        this.size++;
+
+        return true;
     }
 
     @Override
     public boolean add(int index, E element) {
-        return false;
+        validateIndex(index);
+
+        if (this.size == this.elements.length) {
+            grow();
+        }
+
+        shiftRight(index);
+        this.elements[index] = element;
+        size++;
+
+        return true;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        validateIndex(index);
+
+        return getElement(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        validateIndex(index);
+
+        E oldElement = getElement(index);
+        this.elements[index] = element;
+
+        return oldElement;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        validateIndex(index);
+
+        E elementToRemove = getElement(index);
+
+        shiftLeft(index);
+        size--;
+
+        if (this.size < this.capacity / 3) {
+            shrink();
+        }
+
+        return elementToRemove;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public int indexOf(E element) {
-        return 0;
+        for (int i = 0; i < this.size; i++) {
+            if (this.elements[i].equals(element)) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     @Override
     public boolean contains(E element) {
-        return false;
+        return this.indexOf(element) != -1;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size();
+            }
+
+            @Override
+            public E next() {
+                return get(index++);
+            }
+        };
+    }
+
+    private void grow() {
+        this.capacity *= 2;
+
+        Object[] tmp = new Object[this.capacity];
+
+        for (int i = 0; i < this.elements.length; i++) {
+            tmp[i] = this.elements[i];
+        }
+
+        this.elements = tmp;
+    }
+
+    private void shrink() {
+        this.elements = Arrays.copyOf(this.elements, this.capacity / 2);
+    }
+
+    private void validateIndex(int index) {
+         if (index < 0 || index >= this.size) {
+             throw new IndexOutOfBoundsException("Index out of bounds: " + index + " for size " + this.size);
+        }
+    }
+
+    private void shiftRight(int index) {
+        for (int i = this.size - 1; i >= index; i--) {
+            this.elements[i + 1] = this.elements[i];
+        }
+    }
+
+    private void shiftLeft(int index) {
+        for (int i = index; i < this.size - 1; i++) {
+            this.elements[i] = this.elements[i + 1];
+        }
+    }
+
+    private E getElement(int index) {
+        return (E) this.elements[index];
     }
 }
